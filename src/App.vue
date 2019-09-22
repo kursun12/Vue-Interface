b130801a471bf506ad1bd590b459c7ee602761cd
kursun12 <salih_kara@hotmail.nl>
2019-09-22 23:54:26 +0200

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="main-app" class="container">
     <div class="row justify-content-center">
-      <add-appointment/>
-      <appointment-list v-bind:appointments="appointments" @remove="removeItem" @edit="editItem" />
+      <add-appointment @add="addItem" />
+      <search-appointments @searchRecords="searchAppointments"/>
+      <appointment-list v-bind:appointments="searchedApts" @remove="removeItem" @edit="editItem" />
     </div>
   </div>
 </template>
@@ -11,20 +12,26 @@
 import axios from "axios";
 import AppointmentList from "./components/AppointmentList";
 import AddAppointment from "./components/AddAppointment";
+import SearchAppointments from "./components/SearchAppointments";
 import _ from "lodash";
+
 export default {
   name: "MainApp",
   data: function() {
     return {
       title: "Appointment List",
       appointments: [],
+      searchTerms: "",
       aptIndex: 0
     };
   },
+
   components: {
     AppointmentList,
-    AddAppointment
+    AddAppointment,
+    SearchAppointments
   },
+
   mounted() {
     axios.get("./data/appointments.json").then(
       response =>
@@ -35,12 +42,33 @@ export default {
         }))       
     );
   },
+
+  computed : {
+    searchedApts: function() {
+      return this.appointments.filter(item => {
+        return(
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        );
+      });
+    }
+  },
+  
   methods: {
     removeItem: function(apt) {
       return (this.appointments = _.without(this.appointments, apt));
     },
     editItem: function(id, field, text) {
       const aptIndex = _.findIndex(this.appointments, {aptId: id}); this.appointments[aptIndex] [field] = text;
+    },
+    addItem: function(apt) {
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
+    },
+    searchAppointments : function(terms) {
+      this.searchTerms = terms;
     }
   }
 };
--- src/components/AddAppointment.vue
+++ src/components/AddAppointment.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="col-12">
     <div class="card textcenter mt-3">
-      <div class="card-header bg-primary text-white">
+      <div class="card-header bg-primary text-white"
+      @click="hidepanel=!hidepanel">
         <font-awesome-icon icon="plus" class="mr-3" />Add Appointment
       </div>
     </div>
     <!-- panel-heading -->
 
-    <div class="panel-body">
-      <form class="add-appointment form-horizontal">
+    <div class="panel-body" v-bind:class="{'d-none': hidepanel}">
+      <form class="add-appointment form-horizontal" 
+        @submit.prevent="requestAdd">
         <div class="form-group">
           <label class="col-sm-2 control-label" for="petName">Pet Name</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="petName" placeholder="Pet's Name" />
+            <input type="text" class="form-control" id="petName" placeholder="Pet's Name"
+              v-model="formData.petName" />
           </div>
           <!-- col-sm-10 -->
         </div>
@@ -21,7 +24,8 @@
         <div class="form-group">
           <label class="col-sm-2 control-label" for="petOwner">Pet Owner</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="petOwner" placeholder="Owner's Name" />
+            <input type="text" class="form-control" id="petOwner" placeholder="Owner's Name" 
+              v-model="formData.ownerName"/>
           </div>
           <!-- col-sm-10 -->
         </div>
@@ -30,12 +34,14 @@
         <div class="form-group">
           <label class="col-sm-2 control-label" for="aptDate">Date</label>
           <div class="col-sm-4">
-            <input type="date" class="form-control" id="aptDate" />
+            <input type="date" class="form-control" id="aptDate" 
+              v-model="formData.aptData"/>
           </div>
           <!-- col-sm-4 -->
           <label class="col-sm-2 control-label" for="aptTime">Time</label>
           <div class="col-sm-4">
-            <input type="time" class="form-control" id="aptTime" />
+            <input type="time" class="form-control" id="aptTime" 
+              v-model="formData.aptTime"/>
           </div>
           <!-- col-sm-4 -->
         </div>
@@ -50,6 +56,7 @@
               cols="50"
               id="aptNotes"
               placeholder="Appointment Notes"
+              v-model="formData.aptNotes"
             ></textarea>
           </div>
           <!-- col-sm-10 -->
@@ -75,13 +82,32 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 export default {
   name: "AddAppointment",
   data() {
-    return {};
+    return {
+      formData: [],
+      hidepanel: true
+    };
   },
   components: {
     FontAwesomeIcon
+  },
+  methods: {
+    requestAdd: function() {
+      this.formData = {
+        petName: this.formData.petName,
+        petOwner: this.formData.ownerName,
+        aptDate: this.formData.aptData + ' ' + this.formData.aptTime,
+        aptNotes: this.formData.aptNotes,
+      };
+      this.$emit("add", this.formData);
+      this.formData=[];
+      this.hidepanel = true;
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+.card-header {
+  cursor: pointer;
+}
 </style>
